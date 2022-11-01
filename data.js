13216eff940179b26b8b1623190fe9e029b6fc98
rishikeshthakare10 <117150968+rishikeshthakare10@users.noreply.github.com>
2022-11-01 17:04:37 +0530

    new files

---
+++ data.js
@@ -0,0 +1,28 @@
+let movies = [
+  {
+    name: "Attack On Titan",
+    des: "a civilization inside three walls, the last location where humans still live. Over one hundred years ago, humanity was driven to the brink of extinction after the emergence of humanoid giants called Titans, who attack and eat humans on sight.",
+    image: "images/5.jpg"
+  },
+  {
+    name: "Jujutsu Kaisen:Movie 0",
+    des: "Yuta Okkotsu is a timid, 16-year-old, high-school student who is haunted by Rika Orimoto, the cursed spirit of his childhood friend who died six years prior; they had promised to get married when they grew up. Whenever Yuta is bullied, Rika comes to his defense and violently assaults his attackers.",
+    image: "images/a10.jpg"
+  },
+  {
+    name: "Violet Evergarden",
+    des: "Violet is a newcomer to the CH Postal Company, and works as an Auto Memory Doll—a ghostwriter for people who cannot write or are looking for help expressing their emotions in letters.",
+    image: "images/slider 3.png"
+  },
+  {
+    name: "Demon Slayer",
+    des: "It follows teenage Tanjiro Kamado, who strives to become a demon slayer after his family was slaughtered and his younger sister, Nezuko, turned into a demon.",
+    image: "images/2.png"
+  },
+  {
+    name: "Black Clover",
+    des: "Asta and Yuno are orphans raised in the outskirts of the Clover Kingdom. In a world where people are born with magic, Asta was born without any. In contrast, Yuno was born a prodigy with above average magic power and the talent to control it. Additionally, in their coming of age ceremony, Yuno receives a rare four-leaf clover grimoire, like the original Wizard King. On the other hand, Asta received a tattered grimoire where a Devil lies within, a five-leaf clover grimoire.",
+    image: "images/4.png"
+  }
+]
+
